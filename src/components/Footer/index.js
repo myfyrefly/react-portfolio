d67dcdd103f2be54f146cc4b4ec9efb034b9ceb2
sofiaM <smejia03@gmail.com>
2020-07-20 13:56:27 -0500

    LinkedIn icon and link added

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "./style.css";
+import LinkedInimg from "../images/linkedIn.png";
 
 const Footer = () => {
   return (
     <div className="footer">
-      <span>Sofia Mejia 2020</span>
+      <span>
+        Sofia Mejia 2020{" "}
+        <a
+          href="https://www.linkedin.com/in/sofia-mejia-344977a5/"
+          target="_blank"
+        >
+          {" "}
+          <img src={LinkedInimg} alt="LinkedIn icon" />{" "}
+        </a>
+      </span>
     </div>
   );
 };
