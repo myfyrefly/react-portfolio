a9d6a19a094de909c264ad2c09b8934c75b89337
sofiaM <smejia03@gmail.com>
2020-07-20 14:51:53 -0500

    twitter icon and link added

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,20 +1,21 @@
 import React from "react";
 import "./style.css";
 import LinkedInimg from "../images/linkedIn.png";
+import TwitterIcon from "../images/twitter02.png";
 
 const Footer = () => {
   return (
     <div className="footer">
-      <span>
-        Sofia Mejia 2020{" "}
-        <a
-          href="https://www.linkedin.com/in/sofia-mejia-344977a5/"
-          target="_blank"
-        >
-          {" "}
-          <img src={LinkedInimg} alt="LinkedIn icon" />{" "}
-        </a>
-      </span>
+      Sofia Mejia 2020{" "}
+      <a
+        href="https://www.linkedin.com/in/sofia-mejia-344977a5/"
+        target="_blank"
+      >
+        <img src={LinkedInimg} alt="LinkedIn icon" />
+      </a>
+      <a href="https://twitter.com/myfyrefly" target="_blank">
+        <img src={TwitterIcon} alt="Twitter icon" />
+      </a>
     </div>
   );
 };
