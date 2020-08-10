cd673bef282d1b4b63570c83918757a5fd9c9687
sofiaM <smejia03@gmail.com>
2020-08-10 15:15:48 -0500

    added link component from react router dom

--- src/components/AppBar/index.js
+++ src/components/AppBar/index.js
@@ -1,11 +1,7 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
+import { AppBar, Toolbar, Typography } from "@material-ui/core/";
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,6 +13,11 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
+  linkStyle: {
+    margin: 20,
+    color: "white",
+    textDecoration: "none",
+  },
 }));
 
 export default function NavAppBar() {
@@ -26,18 +27,12 @@ export default function NavAppBar() {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
+          <Link to="/" className={classes.linkStyle}>
+            <Typography variant="h5">About</Typography>
+          </Link>
+          <Link to="/projects" className={classes.linkStyle}>
+            <Typography variant="h5">Projects</Typography>
+          </Link>
         </Toolbar>
       </AppBar>
     </div>
